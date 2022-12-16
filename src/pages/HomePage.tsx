3c4be0e8add73840f231bb4bca0025a0a3452ff8
Andrii <badekha@gmail.com>
2022-12-16 17:11:14 +0200

    implemented logic for displaying and loading user repositories

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { useSearchUsersQuery } from "../store/github/github.api"
+import { useLazyGetUserReposQuery, useSearchUsersQuery } from "../store/github/github.api"
 import { useDebounce } from '../hooks/debounce'
 
 export function HomePage() {
@@ -10,10 +10,15 @@ export function HomePage() {
     skip: debounced.length < 3,
     refetchOnFocus: true
   })
+  const [fetchRepos, { isLoading: areReposLoading, data: repos }] = useLazyGetUserReposQuery()
 
   useEffect(() => {
     setDropdown(debounced.length > 3 && data?.length! > 0)
   }, [debounced, data])
+
+  const clickHandler =(username: string) => {
+    fetchRepos(username)
+  }
 
   return (
     <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
@@ -35,11 +40,17 @@ export function HomePage() {
             <li
               key={user.id}
               className="py-2 px-4 hover:bg-gray-500 hover:text-white transition-colors cursor-pointer"
+              onClick={() => clickHandler(user.login)}
             >
               {user.login}
             </li>
           ))}
-        </ul>} 
+        </ul>}
+
+        <div className="container">
+          { areReposLoading && <p className="text-center">Repos are loading...</p> }
+          { repos?.map(repo => <p>repo.url</p>)}
+        </div>
       </div>
       
     </div>
