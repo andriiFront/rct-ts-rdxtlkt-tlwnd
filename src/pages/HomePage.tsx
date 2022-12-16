2f62e4fe89d02ddf4d13b75a96e0bc74bab1b185
Andrii <badekha@gmail.com>
2022-12-16 22:37:24 +0200

    implemented display of basic data of user repositories on cards

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import { useLazyGetUserReposQuery, useSearchUsersQuery } from "../store/github/github.api"
 import { useDebounce } from '../hooks/debounce'
+import { RepoCard } from '../components/RepoCard'
 
 export function HomePage() {
   const [search, setSearch] = useState('')
@@ -17,7 +18,8 @@ export function HomePage() {
   }, [debounced, data])
 
   const clickHandler =(username: string) => {
-    fetchRepos(username)
+    fetchRepos(username);
+    setDropdown(false)
   }
 
   return (
@@ -49,7 +51,7 @@ export function HomePage() {
 
         <div className="container">
           { areReposLoading && <p className="text-center">Repos are loading...</p> }
-          { repos?.map(repo => <p>repo.url</p>)}
+          { repos?.map(repo => <RepoCard repo={repo} key={repo.id} />)}
         </div>
       </div>
       
