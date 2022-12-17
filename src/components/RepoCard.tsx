43bc03e4a23278181b23cead696e75601c415933
Andrii <badekha@gmail.com>
2022-12-17 13:34:09 +0200

    implemented adding/removing repositories to favorites

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,6 +1,28 @@
+import { useState } from 'react'
+import { useActions } from "../hooks/actions";
+import { useAppSelector } from "../hooks/redux";
 import { IRepo } from "../models/models";
 
 export function RepoCard({ repo }: { repo: IRepo }) {
+  const { addFavourite, removeFavourite } = useActions()
+  const { favourites } = useAppSelector(state => state.gitHub)
+
+  const [isFav, setIsFav] = useState(favourites.includes(repo.html_url))
+
+  const addToFavourite = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    addFavourite(repo.html_url)
+    setIsFav(true)
+  }
+
+  const removeFromFavourite = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+
+    removeFavourite(repo.html_url)
+    setIsFav(false)
+  }
+
   return (
     <div className="border py-3 px-5 rounded mb-2 hover:shadow-md hover:bg-gray-100 transition-all">
       <a href={repo.html_url} target="_blank">
@@ -10,8 +32,17 @@ export function RepoCard({ repo }: { repo: IRepo }) {
           Watchers: <span className="font-bold">{repo.watchers}</span>
         </p>
         <p className="text-sm font-thin">{repo?.description}</p>
+
+        {!isFav && <button
+          className="py-2 px-4 mr-2 bg-yellow-400 rounded hover:shadow-md transition-all"
+          onClick={addToFavourite}
+        >Add</button>}
+        
+        {isFav && <button
+          className="py-2 px-4 bg-red-400 rounded hover:shadow-md transition-all"
+          onClick={removeFromFavourite}
+        >Remove</button>}
       </a>
-      
     </div>
   )
 }
--- src/pages/FavouritesPage.tsx
+++ src/pages/FavouritesPage.tsx
@@ -1,6 +1,21 @@
+import { useAppSelector } from "../hooks/redux"
+
 export function FavouritesPage() {
+  const { favourites } = useAppSelector(state => state.gitHub)
+
+  if (favourites.length === 0) {
+    return <p className="text-center">No items</p>
+  }
+
   return (
-    <div>
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      <ul className="list-none">
+        {favourites.map(f => (
+          <li key={f}>
+            <a href={f} target="_blank">{f}</a>
+          </li>
+        ))}
+      </ul>
       Fav
     </div>
   )
